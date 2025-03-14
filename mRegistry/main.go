f3e65a6f8f0310bfe784a0036a37eb35cb0c31c2
pekls563 <p1650191@gmail.com>
2025-03-15 02:18:24 +0800

    Update main.go

--- mRegistry/main.go
+++ mRegistry/main.go
@@ -9,7 +9,7 @@ import (
 //启动注册中心,9091
 
 //web获取服务列表GET，服务心跳检查POST
-var registryAddr = "http://localhost:9091/_geerpc_/registry"
+var registryAddr = "http://localhost:9091/krpc_/registry"
 
 func startRegistry() {
 	l, _ := net.Listen("tcp", ":9091")
